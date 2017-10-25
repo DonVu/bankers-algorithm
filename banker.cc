23e62ee6dca46e38441db5dfc77b42b5aa343a33
Don Vu <don.vu1212@gmail.com>
2017-10-24 19:31:49 -0700

    Create initial code

---
+++ banker.cc
@@ -0,0 +1,14 @@
+//  Don Vu
+//  
+//  This is a multithreaded program that implements
+//  the banker's algorithm.
+
+#include <iostream>
+using namespace std;
+
+int main (int argc, char *argv[]) {
+  if (argc != 4) {
+    cerr << "Usage of the program is: " << argv[0] 
+      << " resourcenum1 resourcenum2 resourcenum3\n";
+  }
+}
