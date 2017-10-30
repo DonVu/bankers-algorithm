00b95eeb83e4afa70f726585251a73ff4f4d85c5
Don Vu <don.vu1212@gmail.com>
2017-10-29 19:28:24 -0700

    Add Banker's data structures

--- banker.cc
+++ banker.cc
@@ -6,6 +6,21 @@
 #include <iostream>
 using namespace std;
 
+#define NUMBER_OF_CUSTOMERS 5
+#define NUMBER_OF_RESOURCES 3
+
+//  the available amount of resources
+int available[NUMBER_OF_RESOURCES];
+
+//  the maximum demand for each customer
+int maximum[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES];
+
+//  the amount currently allocated to each customer
+int allocation[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES];
+
+//  the remaining need of each customer
+int need[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES];
+
 int main (int argc, char *argv[]) {
   if (argc != 4) {
     cerr << "Usage of the program is: " << argv[0] 
