c019d6461a91a6a575a182ec5add27f47de36933
Don Vu <don.vu1212@gmail.com>
2017-10-31 21:18:15 -0700

    Implement the system Safety algorithm

--- banker.cc
+++ banker.cc
@@ -31,6 +31,8 @@ int ReleaseResources(int customernum, int release[]);
 
 void PrintTable(int table[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES]);
 
+bool IsSystemSafe();
+
 int main (int argc, char *argv[]) {
   if (argc != 6) {
     cerr << "Usage of the program is: " << argv[0] 
@@ -69,7 +71,7 @@ int main (int argc, char *argv[]) {
   }
 
   customerfile.close();
-  PrintTable(maximum); 
+  IsSystemSafe(); 
 }
 
 int RequestResources(int customernum, int request[]) {
@@ -92,3 +94,51 @@ void PrintTable(int table[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES]) {
     cout << endl; 
   }
 }
+
+
+//  continually loops around until
+//  it cannot find a process to 
+//  allocate resources 
+bool IsSystemSafe() {
+  int work[NUMBER_OF_RESOURCES];
+
+  for (int i = 0; i < NUMBER_OF_RESOURCES; ++i)
+    work[i] = available[i];
+
+  bool finish[NUMBER_OF_CUSTOMERS];
+  for (int i = 0; i < NUMBER_OF_CUSTOMERS; ++i)
+    finish[i] = false;
+  
+  bool workdone = true;
+  while (workdone) {
+    workdone = false;
+    for (int i = 0; i < NUMBER_OF_CUSTOMERS; ++i) {
+      bool enoughresources = true;
+     
+      for (int j = 0; j < NUMBER_OF_RESOURCES; ++i) {
+        if (need[i][j] > work[j])
+          enoughresources = false;
+      }
+      
+      //  check if a process can be satisfied with 
+      //  resources available to the system and 
+      //  simulate assigning resources and freeing up
+      //  allocated resources
+      if (finish[i] == false && enoughresources) {
+        for (int k = 0; k < NUMBER_OF_CUSTOMERS; ++k)
+          work[k] += allocation[i][k];
+
+        finish[i] = true;
+        workdone = true;
+      }
+    }
+  }
+
+  bool IsSafe = true;
+  for (int i = 0; i < NUMBER_OF_CUSTOMERS; ++i)
+    if (finish[i] == false)
+      IsSafe = false;
+
+  return IsSafe;   
+}
+
